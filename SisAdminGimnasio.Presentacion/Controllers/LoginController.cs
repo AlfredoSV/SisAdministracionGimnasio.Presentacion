08df2f35b3edaab4da3b2ededbf3cc28df89c36f
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-10 20:43:12 -0600

    Agregar archivos de proyecto.

---
+++ SisAdminGimnasio.Presentacion/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SisAdminGimnasio.Presentacion.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Ingresar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ValidarUsuario()
+        {
+            return View();
+        }
+    }
+}
---
+++ SisAdminGimnasio.Presentacion/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SisAdminGimnasio.Presentacion.MvcApplication" Language="C#" %>

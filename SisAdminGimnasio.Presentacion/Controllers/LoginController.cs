3c026b39c7b0393020e2d5fc01b3c41e6a7e3441
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-29 12:39:07 -0600

    Se agrega vista con formulario para registrar a nuevo usuario

--- SisAdminGimnasio.Presentacion/Controllers/LoginController.cs
+++ SisAdminGimnasio.Presentacion/Controllers/LoginController.cs
@@ -66,7 +66,7 @@ namespace SisAdminGimnasio.Presentacion.Controllers
         [AllowAnonymous]
         public ActionResult NuevoUsuario()
         {
-            return null;
+            return View();
         }
 
 

1939e1262666a99379b0e3790ebff383145a11bb
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-29 18:10:23 -0600

    Se agrega comportamiento a controlador acción GuardarNuevoUsuario

--- SisAdminGimnasio.Presentacion/Controllers/LoginController.cs
+++ SisAdminGimnasio.Presentacion/Controllers/LoginController.cs
@@ -76,10 +76,46 @@ namespace SisAdminGimnasio.Presentacion.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public ActionResult GuardarNuevoUsuario()
+        public async Task<ActionResult> GuardarNuevoUsuario(UsuarioViewModel usuarioModelView)
         {
 
-            return null;
+            var urlAuth = "https://localhost:44316/api/UsuarioAuth/UsuarioAuth";
+            var json = "{\"CorreoUsuario\":\"prueba123@outlook.com\",\"ContrasenaUsuario\":\"prueba123\"}";
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    var cliente = new HttpClient();
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var peticion = await cliente.PostAsync(urlAuth, content);
+
+                    if (peticion.IsSuccessStatusCode)
+                    {
+                        var res = await peticion.Content.ReadAsStringAsync();
+
+                        if (usuarioModelView.Recuerdame)
+                        {
+                            var cookie = new HttpCookie("usuario");
+                            cookie.Value = "1244";
+                            HttpContext.Response.Cookies.Add(cookie);
+
+                        }
+                        HttpContext.Session.Add("usuarioSess", Guid.NewGuid());
+                        return RedirectToAction("Inicio", "Inicio");
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                return View("Error");
+            }
+
+            return View("NuevoUsuario", usuarioModelView);
         }
 
 

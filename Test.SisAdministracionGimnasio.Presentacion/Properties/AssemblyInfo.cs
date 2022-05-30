a790b2258de86d62340a8c5553d8f71ac9063336
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-05-29 20:34:25 -0500

    Se agrega proyecto de pruebas

---
+++ Test.SisAdministracionGimnasio.Presentacion/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test.SisAdministracionGimnasio.Presentacion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Test.SisAdministracionGimnasio.Presentacion")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ddacaa8a-9050-4a75-9d7c-941e4e8032f6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
